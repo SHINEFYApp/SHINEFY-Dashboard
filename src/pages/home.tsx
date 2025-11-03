1fc1e9144177ff7dbb38dd326ed4c0eabca5c977
mohamed kamal <mohamedjop0100@gmail.com>
2025-11-03 19:18:18 +0200

    fix: make translation change instantly without reload

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,25 @@
-import { t } from "i18next";
+import { useTranslation } from "react-i18next";
 import { Button } from "../components/ui/button";
 import i18n from "../i18n";
 
-export default function Home(){
-    return(
-        <div>
-            hello from home page
-            <div className="p-4">
-                test translate
-                <h1>{t("welcome")}</h1>
-                <Button
-                    className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded"
-                    onClick={() => i18n.changeLanguage(i18n.language === "en" ? "ar" : "en")}
-                >
-                    {t("click")}
-                </Button>
-            </div>
-        </div>
-    )
+export default function Home() {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      hello from home page
+      <div className="p-4">
+        test translate
+        <h1>{t("welcome")}</h1>
+        <Button
+          className="bg-blue-500 cursor-pointer text-white px-4 py-2 rounded"
+          onClick={() =>
+            i18n.changeLanguage(i18n.language === "en" ? "ar" : "en")
+          }
+        >
+          {t("click")}
+        </Button>
+      </div>
+    </div>
+  );
 }
